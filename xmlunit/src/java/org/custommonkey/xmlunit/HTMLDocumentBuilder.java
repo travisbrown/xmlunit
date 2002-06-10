ffab4d5ebb5ca550fe368ca0331eda59bebcff59
timbacon <timbacon@458802cb-8328-0410-b1d9-a76001e3bb52>
2002-06-10 20:38:10 +0000

    0.6 release
    git-svn-id: https://xmlunit.svn.sourceforge.net/svnroot/xmlunit/trunk@37 458802cb-8328-0410-b1d9-a76001e3bb52

--- xmlunit/src/java/org/custommonkey/xmlunit/HTMLDocumentBuilder.java
+++ xmlunit/src/java/org/custommonkey/xmlunit/HTMLDocumentBuilder.java
@@ -1,3 +1,39 @@
+/*
+******************************************************************
+Copyright (c) 2001, Jeff Martin, Tim Bacon
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of the xmlunit.sourceforge.net nor the names
+      of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written
+      permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+******************************************************************
+*/
+
 package org.custommonkey.xmlunit;
 
 import java.io.IOException;
@@ -26,6 +62,7 @@ import org.xml.sax.helpers.AttributesImpl;
  * 'XHTML' along the way with the help of a TolerantSaxDocumentBuilder and
  * the Swing html parser classes.
  * This allows XML assertions to be made against badly formed HTML.
+ * <br />Examples and more at <a href="http://xmlunit.sourceforge.net"/>xmlunit.sourceforge.net</a>
  * @todo Add examples of usage with HTMLUnit...
  * @see TolerantSaxDocumentBuilder
  */
