12cd88b63eab211f86d376bad8e3c87b707ce91b
timbacon <timbacon@458802cb-8328-0410-b1d9-a76001e3bb52>
2002-12-27 17:46:18 +0000

    Incorporated patch from ludovicc (int constant values to enable switch statements on Difference.getId() method return value)
    git-svn-id: https://xmlunit.svn.sourceforge.net/svnroot/xmlunit/trunk@49 458802cb-8328-0410-b1d9-a76001e3bb52

--- xmlunit/src/java/org/custommonkey/xmlunit/DifferenceConstants.java
+++ xmlunit/src/java/org/custommonkey/xmlunit/DifferenceConstants.java
@@ -42,87 +42,135 @@ package org.custommonkey.xmlunit;
  * <br />Examples and more at <a href="http://xmlunit.sourceforge.net"/>xmlunit.sourceforge.net</a>
  */
 public interface DifferenceConstants {
+	/** Comparing an implied attribute value against an explicit value */
+	int ATTR_VALUE_EXPLICITLY_SPECIFIED_ID = 1;
+	/** Comparing 2 elements and one has an attribute the other does not */
+	int ATTR_NAME_NOT_FOUND_ID = 2;
+	/** Comparing 2 attributes with the same name but different values */
+	int ATTR_VALUE_ID = 3;
+	/** Comparing 2 attribute lists with the same attributes in different sequence */
+	int ATTR_SEQUENCE_ID = 4;
+	/** Comparing 2 CDATA sections with different values */
+	int CDATA_VALUE_ID = 5;
+	/** Comparing 2 comments with different values */
+	int COMMENT_VALUE_ID = 6;
+	/** Comparing 2 document types with different names */
+	int DOCTYPE_NAME_ID = 7;
+	/** Comparing 2 document types with different public identifiers */
+	int DOCTYPE_PUBLIC_ID_ID = 8;
+	/** Comparing 2 document types with different system identifiers */
+	int DOCTYPE_SYSTEM_ID_ID = 9;
+	/** Comparing 2 elements with different tag names */
+	int ELEMENT_TAG_NAME_ID = 10;
+	/** Comparing 2 elements with different number of attributes */
+	int ELEMENT_NUM_ATTRIBUTES_ID = 11;
+	/** Comparing 2 processing instructions with different targets */
+	int PROCESSING_INSTRUCTION_TARGET_ID = 12;
+	/** Comparing 2 processing instructions with different instructions */
+	int PROCESSING_INSTRUCTION_DATA_ID = 13;
+	/** Comparing 2 different text values */
+	int TEXT_VALUE_ID = 14;
+	/** Comparing 2 nodes with different namespace prefixes */
+	int NAMESPACE_PREFIX_ID = 15;
+	/** Comparing 2 nodes with different namespace URIs */
+	int NAMESPACE_URI_ID = 16;
+	/** Comparing 2 nodes with different node types */
+	int NODE_TYPE_ID = 17;
+	/** Comparing 2 nodes but only one has any children*/
+	int HAS_CHILD_NODES_ID = 18;
+	/** Comparing 2 nodes with different numbers of children */
+	int CHILD_NODELIST_LENGTH_ID = 19;
+	/** Comparing 2 nodes with children whose nodes are in different sequence*/
+	int CHILD_NODELIST_SEQUENCE_ID = 20;
+	/** Comparing 2 Documents only one of which has a doctype */
+	int HAS_DOCTYPE_DECLARATION_ID = 21;
+	
     /** Comparing an implied attribute value against an explicit value */
-    public static final Difference ATTR_VALUE_EXPLICITLY_SPECIFIED =
-        new Difference(1, "attribute value explicitly specified", true);
+	public static final Difference ATTR_VALUE_EXPLICITLY_SPECIFIED =
+        new Difference(ATTR_VALUE_EXPLICITLY_SPECIFIED_ID, 
+			"attribute value explicitly specified", true);
 
     /** Comparing 2 elements and one has an attribute the other does not */
     public static final Difference ATTR_NAME_NOT_FOUND =
-        new Difference(2, "attribute name");
+        new Difference(ATTR_NAME_NOT_FOUND_ID, "attribute name");
 
     /** Comparing 2 attributes with the same name but different values */
     public static final Difference ATTR_VALUE =
-        new Difference(3, "attribute value");
+        new Difference(ATTR_VALUE_ID, "attribute value");
 
     /** Comparing 2 attribute lists with the same attributes in different sequence */
     public static final Difference ATTR_SEQUENCE =
-        new Difference(4, "sequence of attributes", true);
+        new Difference(ATTR_SEQUENCE_ID, "sequence of attributes", true);
 
     /** Comparing 2 CDATA sections with different values */
     public static final Difference CDATA_VALUE =
-        new Difference(5, "CDATA section value");
+        new Difference(CDATA_VALUE_ID, "CDATA section value");
 
     /** Comparing 2 comments with different values */
     public static final Difference COMMENT_VALUE =
-        new Difference(6, "comment value");
+        new Difference(COMMENT_VALUE_ID, "comment value");
 
     /** Comparing 2 document types with different names */
     public static final Difference DOCTYPE_NAME =
-        new Difference(7, "doctype name");
+        new Difference(DOCTYPE_NAME_ID, "doctype name");
 
     /** Comparing 2 document types with different public identifiers */
     public static final Difference DOCTYPE_PUBLIC_ID =
-        new Difference(8, "doctype public identifier");
+        new Difference(DOCTYPE_PUBLIC_ID_ID, "doctype public identifier");
 
     /** Comparing 2 document types with different system identifiers */
     public static final Difference DOCTYPE_SYSTEM_ID =
-        new Difference(9, "doctype system identifier", true);
+        new Difference(DOCTYPE_SYSTEM_ID_ID, "doctype system identifier", true);
 
     /** Comparing 2 elements with different tag names */
     public static final Difference ELEMENT_TAG_NAME =
-        new Difference(10, "element tag name");
+        new Difference(ELEMENT_TAG_NAME_ID, "element tag name");
 
     /** Comparing 2 elements with different number of attributes */
     public static final Difference ELEMENT_NUM_ATTRIBUTES =
-        new Difference(11, "number of element attributes");
+        new Difference(ELEMENT_NUM_ATTRIBUTES_ID, "number of element attributes");
 
     /** Comparing 2 processing instructions with different targets */
     public static final Difference PROCESSING_INSTRUCTION_TARGET =
-        new Difference(12, "processing instruction target");
+        new Difference(PROCESSING_INSTRUCTION_TARGET_ID, 
+			"processing instruction target");
 
     /** Comparing 2 processing instructions with different instructions */
     public static final Difference PROCESSING_INSTRUCTION_DATA =
-        new Difference(13, "processing instruction data");
+        new Difference(PROCESSING_INSTRUCTION_DATA_ID, 
+			"processing instruction data");
 
     /** Comparing 2 different text values */
     public static final Difference TEXT_VALUE =
-        new Difference(14, "text value");
+        new Difference(TEXT_VALUE_ID, "text value");
 
     /** Comparing 2 nodes with different namespace prefixes */
     public static final Difference NAMESPACE_PREFIX =
-        new Difference(15, "namespace prefix", true);
+        new Difference(NAMESPACE_PREFIX_ID, "namespace prefix", true);
 
     /** Comparing 2 nodes with different namespace URIs */
     public static final Difference NAMESPACE_URI =
-        new Difference(16, "namespace URI");
+        new Difference(NAMESPACE_URI_ID, "namespace URI");
 
     /** Comparing 2 nodes with different node types */
     public static final Difference NODE_TYPE =
-        new Difference(17, "node type");
+        new Difference(NODE_TYPE_ID, "node type");
 
     /** Comparing 2 nodes but only one has any children*/
     public static final Difference HAS_CHILD_NODES =
-        new Difference(18, "presence of child nodes to be");
+        new Difference(HAS_CHILD_NODES_ID, "presence of child nodes to be");
 
     /** Comparing 2 nodes with different numbers of children */
     public static final Difference CHILD_NODELIST_LENGTH =
-        new Difference(19, "number of child nodes");
+        new Difference(CHILD_NODELIST_LENGTH_ID, "number of child nodes");
 
     /** Comparing 2 nodes with children whose nodes are in different sequence*/
     public static final Difference CHILD_NODELIST_SEQUENCE =
-        new Difference(20, "sequence of child nodes", true);
+        new Difference(CHILD_NODELIST_SEQUENCE_ID, 
+			"sequence of child nodes", true);
     
     /** Comparing 2 Documents only one of which has a doctype */
     public static final Difference HAS_DOCTYPE_DECLARATION = 
-        new Difference(21, "presence of doctype declaration", true);
+        new Difference(HAS_DOCTYPE_DECLARATION_ID, 
+			"presence of doctype declaration", true);
 }
