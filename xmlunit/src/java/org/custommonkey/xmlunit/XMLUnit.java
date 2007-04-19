a55c50418c1086d4c360cb35d55c2042deb7295a
bodewig <bodewig@458802cb-8328-0410-b1d9-a76001e3bb52>
2007-04-19 04:00:56 +0000

    bump version
    git-svn-id: https://xmlunit.svn.sourceforge.net/svnroot/xmlunit/trunk@196 458802cb-8328-0410-b1d9-a76001e3bb52

--- xmlunit/src/java/org/custommonkey/xmlunit/XMLUnit.java
+++ xmlunit/src/java/org/custommonkey/xmlunit/XMLUnit.java
@@ -469,7 +469,7 @@ public final class XMLUnit {
      * @return current version
      */
     public static String getVersion() {
-        return "1.1alpha"; 
+        return "1.1beta2"; 
     }
 
    /**
